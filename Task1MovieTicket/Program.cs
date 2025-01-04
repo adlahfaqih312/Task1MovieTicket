85ec195a2354ffddcdaf3aae84ac81e8572678c2
adlahfaqih312 <adlah.faqih.99@gmail.com>
2025-01-04 14:28:23 +0200

    this is the code

--- Task1MovieTicket/Program.cs
+++ Task1MovieTicket/Program.cs
@@ -4,7 +4,47 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            void discountDay(string d, double a)
+            {
+                if (d == "MONDAY" || d == "TUESDAY" || d == "WEDNESDAY" || d == "THURSDAY")
+                {
+                    a = a - ((a * 10) / 100);
+                    Console.WriteLine($"your ticket price is : {a}");
+                }
+                else if (d == "FRIDAY" || d == "SATURDAY" || d == "SUNDAY")
+                {
+                    a = a;
+                    Console.WriteLine($"your ticket price is : {a}");
+                }
+                else
+                {
+                    Console.WriteLine("You enter a wrong day");
+                }
+            }
+            Console.WriteLine("Please Enter your age : ");
+            int age = int.Parse(Console.ReadLine());
+            double price;
+            Console.WriteLine("Please Enter the day you want : ");
+            string day = Console.ReadLine();
+            string d = day.ToUpper();
+            if (age >= 18 && age < 60)
+            {
+                price = 12;
+                discountDay(d, price);
+
+            }
+            else if (age >= 60)
+            {
+                price = 8;
+                discountDay(d, price);
+            }
+            else
+            {
+                price = 6;
+                discountDay(d, price);
+            }
+
+
         }
     }
 }
